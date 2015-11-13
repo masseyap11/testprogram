d012a306f94c6439055c993b3bc7f442dbc25c17
Angela Massey <amassey@bu.edu>
2015-11-13 13:14:02 -0500

    Added index.php with PDO

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+
+//$host="192.168.0.5"; 
+$host="localhost";
+$user="root";
+$pass="root";
+$db="testprogram";
+try {
+	$PDO=new PDO("mysql:host=$host;dbname=$db", $user, $pass);
+}
+catch(Exception $e) {
+	echo $e->getMessage();
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Hello, Angela!</title>
+</head>
+<body>
+This page will access the database "test program"
+</body>
+</html>
